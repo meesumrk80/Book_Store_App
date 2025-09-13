4699dea9fdd62ddfa3d998b552ac1c7d7648266c
meesumrk80 <meesumrk80@gmail.com>
2025-09-13 16:11:35 +0500

    [order]

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   # Callbacks
   after_create :create_cart
-
+  after_initialize :set_default_role, if: :new_record?
   # Validations
   validates :email, format: {
     with: /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/,
@@ -31,7 +31,9 @@ class User < ApplicationRecord
   end
 
   private
-
+ def set_default_role
+    self.role ||= "customer"
+ end
   def create_cart
     Cart.create(user: self)
   end
